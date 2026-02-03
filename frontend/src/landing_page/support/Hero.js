de91bc051191945348d0814f4ab6f938623fbb74
Chirag Choken <chokenchirag89@gmail.com>
2026-02-04 00:55:42 +0530

    Fix accessibility warnings and prepare production build

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,32 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
     <section className="container-fluid" id="supportHero">
-      <div className="p-5 " id="supportWrapper">
+      <div className="p-5" id="supportWrapper">
         <h4>Support Portal</h4>
-        <a href="">Track Tickets</a>
+
+        {/* Button instead of empty href */}
+        <button className="btn btn-link p-0">Track Tickets</button>
       </div>
+
       <div className="row p-5 m-3">
         <div className="col-6 p-3">
           <h1 className="fs-3">
             Search for an answer or browse help topics to create a ticket
           </h1>
-          <input placeholder="Eg. how do I activate F&O" />
-          <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+
+          <input
+            placeholder="Eg. how do I activate F&O"
+            className="form-control my-3"
+          />
+
+          {/* Internal navigation – safe placeholders */}
+          <ul className="list-unstyled">
+            <li>
+              <Link to="#">Track account opening</Link>
+            </li>
+            <li>
+              <Link to="#">Track segment activation</Link>
+            </li>
+            <li>
+              <Link to="#">Intraday margins</Link>
+            </li>
+            <li>
+              <Link to="#">Kite user manual</Link>
+            </li>
+          </ul>
         </div>
+
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
-              <a href="">Current Takeovers and Delisting - January 2024</a>
+              <Link to="#">
+                Current Takeovers and Delisting – January 2024
+              </Link>
             </li>
             <li>
-              <a href="">Latest Intraday leverages - MIS & CO</a>
+              <Link to="#">Latest Intraday leverages – MIS & CO</Link>
             </li>
           </ol>
         </div>
